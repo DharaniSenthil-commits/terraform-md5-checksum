ab4c09e6d0f9d8655d125b6baadf8dcd6ad7528a
DharaniSenthil-commits <77285951+DharaniSenthil-commits@users.noreply.github.com>
2022-05-23 10:14:00 +0530

    Add files via upload

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -1,17 +1,24 @@
-resource "aws_iam_role_policy" "ec2-role-policy" {
-    name="ec2-role-policy"
-    role="${aws_iam_role.ec2-role.id}"
+resource "aws_iam_policy" "tf_s3_policy" {
+    name="tf_s3_policy"
     policy = file("modules/iam/config_policy/s3-access-policy.json")
 }
 
-resource "aws_iam_role" "ec2-role" {
-  name = "ec2-role"
+resource "aws_iam_role" "tf_ec2_role" {
+  name = "tf_ec2_role"
   description="Role for Ec2"
   assume_role_policy = file("modules/iam/config_policy/ec2-role.json")
 }
 
-resource "aws_iam_instance_profile" "ec2-role" {
-  name="ec2-profile"
-  role="${"aws_iam_role.ec2-role.name"}"
+resource "aws_iam_policy_attachment" "ec2_s3_role"{
+
+  name ="ec2_s3_attachment"
+  roles = ["${aws_iam_role.tf_ec2_role.name}"]
+  policy_arn = aws_iam_policy.tf_s3_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_s3_profile" {
+  
+  name="ec2_s3_profile"
+  role=aws_iam_role.tf_ec2_role.name
 }
 
--- modules/iam/output.tf
+++ modules/iam/output.tf
@@ -1,3 +1,3 @@
 output "instance_profile_name"{
-    value=aws_iam_instance_profile.ec2-role.name
+    value= aws_iam_instance_profile.ec2_s3_profile.name
 }
